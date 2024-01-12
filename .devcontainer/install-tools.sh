09a8473bce2caf6b8fb3a74e9383742fa0c0b1df
Dharmesh Patel <dspatel44@gmail.com>
2024-01-12 17:27:38 +0530

    Fix front-end issue.

--- .devcontainer/install-tools.sh
+++ .devcontainer/install-tools.sh
@@ -21,3 +21,5 @@ set -eux
 
 # Copy the welcome message
 sudo cp .devcontainer/welcome-message.txt /usr/local/etc/vscode-dev-containers/first-run-notice.txt
+SITE_URL="https://${CODESPACE_NAME}-8080.${GITHUB_CODESPACES_PORT_FORWARDING_DOMAIN}"
+sudo sed -i "s!SITE_URL!$SITE_URL!" /usr/local/etc/vscode-dev-containers/first-run-notice.txt
